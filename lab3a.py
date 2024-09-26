acf9c5ff0cf333dd125e76d59a08641245527dd9
Ivan Vassiljenko <ivassiljenko@myseneca.ca>
2024-09-26 11:09:57 -0400

    added lab3a

---
+++ lab3a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+
+# ID: ivassiljenko
+
+def return_text_value():
+    return 'Good Morning Terry'
+
+# return_number_value() function
+
+def return_number_value():
+    return 15  
+
+# Main Program
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
